beeb09b20f47cdbc7f75a494950e89e75bbedcca
Pineapple <Pineapple@Users-MacBook-Pro.local>
2017-07-26 15:10:55 -0700

    move components into seperate files

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,13 @@
-// import React, { Component } from 'react';
+import React from 'react';
 
-// class Search extends Component {
-//     render() {
-//         const { value, onChange } = this.props;
-//         return (
-//             <form>
-//                 <input
-//                     type="text"
-//                     value={value}
-//                     onChange={onChange}
-//                 />
-//             </form>
-//         );
-//     }
-// }
 
-// export default Search; 
+const Search = ({ value, onChange, children }) => 
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+export default Search; 
